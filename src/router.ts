0d0ad1c50e65ab753687b4bbf324c4c50b67ed4a
XXXMrG <xxkeith@outlook.com>
2024-03-25 09:12:21 +0800

    dragt

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import BMView from './components/BM.vue';
+import DayOffView from './components/DayOff.vue';
 import HomeView from './components/Phone.vue';
 
 const routes = [
+    { path: '/day-off', component: DayOffView },
     { path: '/bm', component: BMView },
     { path: '/', component: HomeView },
 ];
